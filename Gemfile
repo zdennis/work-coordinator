4ef46124bf0f746117193c243cb72ffdab9a1633
Zach Dennis <zach.dennis@gmail.com>
2026-08-03 19:47:20 -0400

    chore: add rubocop with pre-commit hook enforcing lint on staged files

--- Gemfile
+++ Gemfile
@@ -5,11 +5,13 @@ source "https://rubygems.org"
 ruby ">= 3.2"
 
 gem "activerecord", "~> 7.1"
-gem "term-ansicolor", "~> 1.11"
 gem "sqlite3", "~> 1.7"
+gem "term-ansicolor", "~> 1.11"
 
 group :development, :test do
-  gem "rspec", "~> 3.13"
-  gem "factory_bot", "~> 6.4"
   gem "database_cleaner-active_record", "~> 2.1"
+  gem "factory_bot", "~> 6.4"
+  gem "rspec", "~> 3.13"
+  gem "rubocop", "~> 1.65", require: false
+  gem "rubocop-rspec", "~> 3.0", require: false
 end
--- lib/work_coordinator/domain/work_item.rb
+++ lib/work_coordinator/domain/work_item.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 module WorkCoordinator
   module Domain
+    WORK_ITEM_STATES = %i[created ready active waiting_for_human waiting_for_resource blocked completed
+                          abandoned].freeze
+    WORK_ITEM_KINDS = %i[jira chore investigation adhoc].freeze
+    WORK_ITEM_PHASES = %i[investigating implementing verifying reviewing].freeze
+
     WorkItem = Data.define(
       :id,
       :title,
@@ -12,22 +19,17 @@ module WorkCoordinator
       :created_at,
       :updated_at
     ) do
-      STATES = %i[created ready active waiting_for_human waiting_for_resource blocked completed abandoned].freeze
-      KINDS = %i[jira chore investigation adhoc].freeze
-      PHASES = %i[investigating implementing verifying reviewing].freeze
+      def state?(check_state) = state == check_state
 
-      def state?(s) = state == s
-
-      def created?           = state?(:created)
-      def ready?             = state?(:ready)
-      def active?            = state?(:active)
-      def waiting_for_human? = state?(:waiting_for_human)
+      def created?              = state?(:created)
+      def ready?                = state?(:ready)
+      def active?               = state?(:active)
+      def waiting_for_human?    = state?(:waiting_for_human)
       def waiting_for_resource? = state?(:waiting_for_resource)
-      def blocked?           = state?(:blocked)
-      def completed?         = state?(:completed)
-      def abandoned?         = state?(:abandoned)
+      def blocked?              = state?(:blocked)
+      def completed?            = state?(:completed)
+      def abandoned?            = state?(:abandoned)
 
-      # Compatibility aliases
       def open?        = created?
       def done?        = completed?
       def in_progress? = active?
